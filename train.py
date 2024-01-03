067b8fa5310be941802205cbb19550c090ff7082
Luigi Sbailo <luigi.sbailo@gmail.com>
2024-01-03 22:32:46 +0100

    rename file to perform trianing

--- train.py
+++ train.py
@@ -65,6 +65,7 @@ if __name__ == '__main__':
     parser.add_argument('--model', required=True)
     parser.add_argument('--sample', required=False)
     parser.add_argument('--lr', required=False)
+    parser.add_argument('--lr-file', required=False)
     parser.add_argument('--etf-metrics', required=False)
     args = parser.parse_args()
 
@@ -74,6 +75,7 @@ if __name__ == '__main__':
     architecture_model = args.model
     sample = args.sample
     lr = args.lr
+    lr_file = args.lr_file
     etf_simplex_metrics = args.etf_metrics
 
     configs =  parse_config(config_file)
@@ -81,29 +83,45 @@ if __name__ == '__main__':
     training_hypers = configs['training']['hypers']
     name_dataset = configs['dataset']['name']    
     if lr:
+        if lr.startswith('.'):
+            lr = '0'+lr
         training_hypers['lr'] = float(lr)
+    if lr_file:
+        with open(lr_file, 'rb') as file:
+            lr_dict = pickle.load(file)
+            training_hypers['lr']=lr_dict[architecture_model]
     if not etf_simplex_metrics:
         etf_simplex_metrics=False
     elif etf_simplex_metrics == 'True' or etf_simplex_metrics == 'true':
         etf_simplex_metrics=True
     elif etf_simplex_metrics == 'False' or etf_simplex_metrics == 'false':
         etf_simplex_metrics=False
-
+  
+    print(architecture_model)
+    print('Learning rate: ', training_hypers['lr']) 
+    
     torch_module= importlib.import_module("torchvision.datasets")
     torch_dataset = getattr(torch_module, name_dataset)
     transform = transforms.Compose([transforms.ToTensor()])
     trainset = torch_dataset (str(dataset_dir), train=True, download=True, transform=transform)
     trainset_mean, trainset_std = compute_mean_std(trainset)
-    transform_normalized = transforms.Compose([
+    transform_train = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(trainset_mean, trainset_std)
+        transforms.Normalize(trainset_mean, trainset_std),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(trainset[0][0][0][0].shape[0], padding=4),
     ])
-    trainset = torch_dataset (str(dataset_dir), train=True, download=True, transform=transform_normalized)
-    testset = torch_dataset (str(dataset_dir), train=False, download=True, transform=transform_normalized)
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(trainset_mean, trainset_std),
+    ])
+
+    trainset = torch_dataset (str(dataset_dir), train=True, download=True, transform=transform_train)
+    testset = torch_dataset (str(dataset_dir), train=False, download=True, transform=transform_test)
 
     in_channels = trainset[0][0].shape[0]  
     num_classes = len(set(trainset.classes))
-
+    
     path_metrics_dir = str(results_dir)
 
     print('Training ' + str(architecture_model) + ' architecture:')
@@ -121,6 +139,7 @@ if __name__ == '__main__':
         classifier = torch.nn.DataParallel(classifier)
         classifier = classifier.to(device)
     
+
     res = Trainer(device=device, 
             network=classifier, 
             trainset=trainset, 
@@ -129,7 +148,8 @@ if __name__ == '__main__':
             model=architecture_model, 
             etfsimplex_metrics=etf_simplex_metrics,
             verbose=verbose).fit()
-        
+    res['training_hypers'] = training_hypers
+    res['architecture'] = architecture    
 
     if sample:    
         file_name = '/res_' + architecture_model + '_' + sample + '.pkl'
