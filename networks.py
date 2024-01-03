dea28b272f3d66e823e235a4aefc7af80360758b
Luigi Sbailo <luigi.sbailo@gmail.com>
2024-01-03 22:28:40 +0100

    fix resnet50

--- networks.py
+++ networks.py
@@ -44,12 +44,16 @@ class Classifier(nn.Module):
         l_nodes = [input_dims] + self.backbone_dense_nodes
  
         for i in range(len(l_nodes)-1):
+            if self.dropout:
+                if len(self.dropout) != len (self.backbone_dense_nodes):
+                    print('ERROR: length of dropout values different from lenght head layers')
+                    sys.exit(1)
+                l_layers.append(nn.Dropout(p=self.dropout[i]))
+
             l_layers.append(nn.Linear(l_nodes[i],l_nodes[i+1]))
             l_layers.append(self.activation())
             if self.architecture_hypers['dense_bn']:
                 l_layers.append(nn.BatchNorm1d(l_nodes[i+1]))
-            if self.dropout>0:
-                l_layers.append(nn.Dropout(p=self.dropout))
 
 
         self.dense_classifier = nn.Sequential(*l_layers)
@@ -81,17 +85,17 @@ class Classifier(nn.Module):
         if self.pen_lin_nodes:
             x_pen = self.pen_layer(x)
             x_output = self.output_layer(x_pen)
-            y = torch.softmax(x_output, dim=-1)
+            # y = torch.softmax(x_output, dim=-1)
         elif self.pen_nonlin_nodes:
             x_pen = self.pen_layer(x)
             x_output = self.output_layer(x_pen)
-            y = torch.softmax(x_output, dim=-1)
+            # y = torch.softmax(x_output, dim=-1)
         else:
             x_pen = x
             x_output = self.output_layer(x_pen)
-            y = torch.softmax(x_output, dim=-1)
-        
-        return y.reshape(-1,self.num_classes), x_pen
+            # y = torch.softmax(x_output, dim=-1)
+            
+        return x_output.reshape(-1,self.num_classes), x_pen
     
 
 class MLPvanilla (Classifier):
@@ -198,79 +202,34 @@ class VGG(Classifier):
 
 
 
-
-# 3x3 convolution
-def conv3x3(in_channels, out_channels, stride=1):
-
-    return nn.Conv2d(in_channels, out_channels, kernel_size=3, 
-                        stride=stride, padding=1, bias=False)
-
-def conv1x1(in_channels, out_channels):
-
-    return nn.Conv2d(in_channels, out_channels, kernel_size=1, 
-                        stride=1, padding=0, bias=False)
-
-# class BasicBlock(nn.Module):
-
-#     def __init__(self, in_channels, out_channels, activation, stride=1, downsample=None):
-#         super(BasicBlock, self).__init__()
-        
-#         self.expansion = 1        
-#         self.conv1 = conv3x3(in_channels, out_channels, stride)
-#         self.bn1 = nn.BatchNorm2d(out_channels)
-#         self.activation = activation(inplace=True)
-#         self.conv2 = conv3x3(out_channels, out_channels)
-#         self.bn2 = nn.BatchNorm2d(out_channels)
-#         self.downsample = downsample
-
-    
-#     def forward(self, x):
-#         residual = x
-#         out = self.conv1(x)
-#         out = self.bn1(out)
-#         out = self.activation(out)
-#         out = self.conv2(out)
-#         out = self.bn2(out)
-#         if self.downsample:
-#             residual = self.downsample(x)
-#         out += residual
-#         out = self.activation(out)
-        # return out
-
-
 class Bottleneck(nn.Module):
-    
-    def __init__(self, in_channels, out_channels, activation, downsample=None, stride=1):
+
+    expansion = 4
+    def __init__(self, in_channels, out_channels, activation, stride=1):
         super(Bottleneck, self).__init__()
-        
-        self.expansion = 4        
-        self.conv1 = conv1x1(in_channels, out_channels)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.activation = activation(inplace=True)
-        self.conv2 = conv3x3(out_channels, out_channels, stride)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        
-        self.conv3 = conv1x1(out_channels, self.expansion*out_channels)
-        self.bn3 = nn.BatchNorm2d(self.expansion*out_channels)
-        self.downsample = downsample
-        self.stride = stride
-        
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            activation(inplace=True),
+            nn.Conv2d(out_channels, out_channels, stride=stride, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            activation(inplace=True),     
+            nn.Conv2d(out_channels, out_channels * Bottleneck.expansion, kernel_size=1, bias=False),
+            nn.BatchNorm2d(self.expansion*out_channels)
+        )
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_channels != out_channels * Bottleneck.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * Bottleneck.expansion, stride=stride, kernel_size=1, bias=False),
+                nn.BatchNorm2d(out_channels * Bottleneck.expansion)
+            )
+        self.activation = activation
+                
     def forward(self, x):
-        identity = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.activation(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.activation(out)
-        out = self.conv3(out)
-        out = self.bn3(out)
-        if self.downsample is not None:
-            identity = self.downsample(x)
-            
-        out += identity
-        out = self.activation(out)
-        return out
+
+        return self.activation(inplace=True)(self.residual_function(x) + self.shortcut(x))
+
 
     
 # ResNet
@@ -280,69 +239,57 @@ class ResNet50(Classifier):
 
         super().__init__( model, architecture, in_channels, num_classes)
         
-        self.expansion = 4
         self.in_channels = 64
 
-        layers = [3, 4, 6, 3]
-        self.make_backbone_layers(Bottleneck, layers)
-        
+        blocks = [3, 4, 6, 3]
+        self.make_backbone_layers(Bottleneck, blocks)
+
         if self.backbone_dense_nodes:
-            self.make_dense_classifier (input_dims=512*self.expansion)
+            self.make_dense_classifier (input_dims=512*Bottleneck.expansion)
             self.pen_layer, self.output_layer = self.make_penultimate(self.backbone_dense_nodes[-1])
         else:
-            self.pen_layer, self.output_layer = self.make_penultimate(input_dims=512*self.expansion)
+            self.pen_layer, self.output_layer = self.make_penultimate(input_dims=512*Bottleneck.expansion)
 
 
-    def make_layer(self, block, out_channels, blocks, stride=1):
-        downsample = None
+    def make_layer(self, block, out_channels, num_blocks, stride=1):
+
+        strides = [stride] + [1] * (num_blocks - 1)
         layers = []
-                
-        if stride != 1 or self.in_channels != out_channels*self.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, out_channels*self.expansion, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(out_channels*self.expansion)
-            )
-            
-        layers.append(block(self.in_channels, out_channels, self.activation, downsample=downsample, stride=stride))
-        self.in_channels = out_channels*self.expansion
-        
-        for i in range(1, blocks):
-            layers.append(block(self.in_channels, out_channels, self.activation))
-        return nn.Sequential(*layers)
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, self.activation, stride))
+            self.in_channels = out_channels * block.expansion
+
+        return nn.Sequential(*layers)                            
 
 
-    def make_backbone_layers(self, block, layers):
+
+    def make_backbone_layers(self, block, blocks):
             
         self.in_channels = 64
-        self.conv = conv3x3(3, 64)
-        self.bn = nn.BatchNorm2d(64)
-        self.max_pool = nn.MaxPool2d(kernel_size = 3, stride=2, padding=1)
-
-        self.layer1 = self.make_layer(block, 64, layers[0])
-        self.layer2 = self.make_layer(block, 128, layers[1], 2)
-        self.layer3 = self.make_layer(block, 256, layers[2], 2)
-        self.layer4 = self.make_layer(block, 512, layers[3], 2)
+        
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            self.activation(inplace=True),
+        )
+        self.layer1 = self.make_layer(block, 64, blocks[0], 1)
+        self.layer2 = self.make_layer(block, 128, blocks[1], 2)
+        self.layer3 = self.make_layer(block, 256, blocks[2], 2)
+        self.layer4 = self.make_layer(block, 512, blocks[3], 2)
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         
-        l_layers = nn.ModuleList ()
-        l_layers.append(self.conv)
-        l_layers.append(self.bn)
-        l_layers.append(self.activation())
-        # l_layers.append(self.max_pool)
-        l_layers.append(self.layer1)
-        l_layers.append(self.layer2)
-        l_layers.append(self.layer3)
-        l_layers.append(self.layer4)
-        l_layers.append(self.avg_pool)
-
-        self.backbone_layers =  nn.Sequential(*l_layers)
 
     def forward(self, x):
+        output = self.layer0(x)
+        output = self.layer1(output)
+        output = self.layer2(output)
+        output = self.layer3(output)
+        output = self.layer4(output)
+        output = self.avg_pool(output)
+        output = output.view(output.size(0), -1)   
+        output = torch.flatten(output, start_dim=1)
 
-        x = self.backbone_layers(x)    
-        x = torch.flatten(x, start_dim=1)
-
-        return self.classifier_forward(x)
+        return self.classifier_forward(output)
 
     
 
